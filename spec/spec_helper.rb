3910f75ccbebf268219a032835145125e95c6d71
Martin Fenner <martin.fenner@datacite.org>
2016-09-03 06:54:26 +0200

    add test coverage badge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,5 +22,6 @@ VCR.configure do |c|
   c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
   c.ignore_localhost = true
+  config.ignore_hosts 'codeclimate.com'
   c.configure_rspec_metadata!
 end
